e1c73742306b534d88e8d60539eda14bd914b48f
andreibromaskevis <126252917+andreibromaskevis@users.noreply.github.com>
2023-06-21 18:26:24 -0300

    Solução1.go

---
+++ Solução1.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//criando as variaveis
+
+	var numeros = [3]int{5, 10, 20}
+	fmt.Println("Os valores no array são", numeros)
+
+	soma := 0
+
+	for _, valor := range numeros {
+		soma += valor
+	}
+	fmt.Printf("A soma dos valores no array é %d", soma)
+
+}
